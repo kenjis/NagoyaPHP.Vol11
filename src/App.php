59c7b46fd95e6a9630ade7553d125bf7896d0855
kenjis <kenji.uui@gmail.com>
2016-04-16 22:02:43 +0900

    Refactor

--- src/App.php
+++ src/App.php
@@ -15,82 +15,12 @@ class App
         $w1 = new Woman(1);
         $this->collection->set($w1);
         
-        foreach ([2,3,4] as $i) {
-            $w = new Woman($i);
-            $w->setMother($w1);
-            $this->collection->set($w);
-        }
-        
-        foreach ([5,6,7] as $i) {
-            $w = new Woman($i);
-            $w->setMother($this->collection->find(2));
-            $this->collection->set($w);
-        }
-        
-        foreach ([8,9,10] as $i) {
-            $w = new Woman($i);
-            $w->setMother($this->collection->find(3));
-            $this->collection->set($w);
-        }
-        
-        foreach ([11,12,13] as $i) {
-            $w = new Woman($i);
-            $w->setMother($this->collection->find(4));
-            $this->collection->set($w);
-        }
-        
-        foreach ([14,15,16] as $i) {
-            $w = new Woman($i);
-            $w->setMother($this->collection->find(5));
-            $this->collection->set($w);
-        }
-        
-        foreach ([17,18,19] as $i) {
-            $w = new Woman($i);
-            $w->setMother($this->collection->find(6));
-            $this->collection->set($w);
-        }
-        
-        foreach ([20,21,22] as $i) {
-            $w = new Woman($i);
-            $w->setMother($this->collection->find(7));
-            $this->collection->set($w);
-        }
-        
-        foreach ([23,24,25] as $i) {
-            $w = new Woman($i);
-            $w->setMother($this->collection->find(8));
-            $this->collection->set($w);
-        }
-        
-        foreach ([26,27,28] as $i) {
-            $w = new Woman($i);
-            $w->setMother($this->collection->find(9));
-            $this->collection->set($w);
-        }
-        
-        foreach ([29,30,31] as $i) {
-            $w = new Woman($i);
-            $w->setMother($this->collection->find(10));
-            $this->collection->set($w);
-        }
-        
-        foreach ([32,33,34] as $i) {
-            $w = new Woman($i);
-            $w->setMother($this->collection->find(11));
-            $this->collection->set($w);
-        }
-        
-        foreach ([35,36,37] as $i) {
-            $w = new Woman($i);
-            $w->setMother($this->collection->find(12));
-            $this->collection->set($w);
-        }
-        
-        foreach ([38,39,40] as $i) {
-            $w = new Woman($i);
-            $w->setMother($this->collection->find(13));
-            $this->collection->set($w);
+        foreach (range(1, 13) as $i) {
+            foreach ([$i*3-1, $i*3, $i*3+1] as $j) {
+                $w = new Woman($j);
+                $w->setMother($this->collection->find($i));
+                $this->collection->set($w);
+            }
         }
     }
 
@@ -105,7 +35,7 @@ class App
     {
         $tmp = explode('->', $input);
         return [
-            $tmp[0], $tmp[1]
+            (int) $tmp[0], (int) $tmp[1]
         ];
     }
 }
